4099ac7d2c0d02f665422c9804bc0ecaac96ab0e
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-05-16 14:28:59 -0700

    Update isreadonly.lua

--- isreadonly.lua
+++ isreadonly.lua
@@ -1,4 +1,4 @@
-function isreadonly(instance, property)
+getgenv().isreadonly = function(instance, property)
 	return not (pcall(function()
 		instance[property] = instance[property] -- errors if readonly, or nonexistant
 	end))
