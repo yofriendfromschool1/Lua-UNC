f25764f7df1dfe416d05004557e13b14804311ef
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-05-17 16:00:36 -0700

    Update mousefunctions.lua

--- mousefunctions.lua
+++ mousefunctions.lua
@@ -46,3 +46,18 @@ getgenv().mousescroll = function(x, y, a)
 	a = a and true or false
 	vim:SendMouseWheelEvent(x, y, a, game)
 end
+getgenv().mousemoverel = function(relx, rely)
+    local vim = game:GetService('VirtualInputManager');
+	local Pos = workspace.CurrentCamera.ViewportSize
+	relx = relx or 0
+	rely = rely or 0
+	local x = Pos.X * relx
+	local y = Pos.Y * rely
+	vim:SendMouseMoveEvent(x, y, game)
+end
+getgenv().mousemoveabs = function(x, y)
+    local vim = game:GetService('VirtualInputManager');
+	x = x or 0
+	y = y or 0
+	vim:SendMouseMoveEvent(x, y, game)
+end
