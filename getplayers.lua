0a763737f0223e37c981bdb3c86cb8ad3c9caa33
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-07-13 23:20:22 -0700

    Add files via upload

---
+++ getplayers.lua
@@ -0,0 +1,8 @@
+getgenv().getplayers() = function()
+    local players = {}
+    for _, x in pairs(game:GetService("Players"):GetPlayers()) do
+        players[x.Name] = x
+    end
+    players["LocalPlayer"] = game:GetService("Players").LocalPlayer
+    return players
+end
