5d9e28a53390541503c536cdfd380a04693f6b62
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-08-04 17:17:43 -0700

    Update getplayer.lua

--- getplayer.lua
+++ getplayer.lua
@@ -1,10 +1,3 @@
-getgenv().getplayer = function(plr)
-    if type(plr) ~= "string" then
-        return error("Invalid Type, must be a string.")
-    end
-    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-        if v.Name == plr or v.DisplayName == plr then
-            return v
-        end
-    end
+getgenv().getplayer = function(name)
+    return not name and getplayers()["LocalPlayer"] or getplayers()[name]
 end
